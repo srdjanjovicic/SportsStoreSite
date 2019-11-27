ab0c6202a01e28fb4c5d513ac9ff996ccff4f138
Srdjan Jovicic <srdjansamja@gmail.com>
2019-11-27 19:34:04 +0100

    Added project

---
+++ SportsStore.WebUI/Controllers/NavController.cs
@@ -0,0 +1,30 @@
+﻿using SportsStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class NavController : Controller
+    {
+		private readonly IProductRepository _repository;
+		public NavController(IProductRepository repository)
+		{
+			_repository = repository;
+		}
+        public PartialViewResult Menu(string category = null)
+		{
+
+			ViewBag.SelectedCategory = category;
+
+			IEnumerable<string> categories = _repository.Products
+				.Select(x => x.Category)
+				.Distinct()
+				.OrderBy(x => x);
+
+			return PartialView(categories);
+		}
+    }
+}
---
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using SportsStore.Domain.Abstract;
+using SportsStore.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+		private readonly IProductRepository _repository;
+		public int PageSize = 4;
+
+		public ProductController(IProductRepository repository)
+		{
+			_repository = repository;
+		}
+
+		public ViewResult List(string category, int page = 1)
+		{
+			var products = _repository.Products
+				.Where(p => category == null || p.Category == category)
+				.OrderBy(p => p.ProductID);
+
+			ProductListViewModel model = new ProductListViewModel()
+			{
+				Products = products.Skip((page - 1) * PageSize).Take(PageSize),
+				PagingInfo = new PagingInfo()
+				{
+					CurrentPage = page,
+					ItemsPerPage = PageSize,
+					TotalItems = products.Count()
+				},
+				CurrentCategory = category
+			};
+
+			return View(model);
+		}
+    }
+}
